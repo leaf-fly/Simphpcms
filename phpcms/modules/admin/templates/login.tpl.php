df5ba6c0c824810f1161b852ac3c9c3191fe84b3
simleaf <814183676@qq.com>
2015-01-10 15:03:03 +0800

    Changes admin login style

--- phpcms/modules/admin/templates/login.tpl.php
+++ phpcms/modules/admin/templates/login.tpl.php
@@ -1,29 +1,25 @@
-<?php defined('IN_ADMIN') or exit('No permission resources.'); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET;?>" />
-<meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
-<title><?php echo L('phpcms_logon')?></title>
-<style type="text/css">
-	div{overflow:hidden; *display:inline-block;}div{*display:block;}
-	.login_box{background:url(<?php echo IMG_PATH?>admin_img/login_bg.jpg) no-repeat; width:602px; height:416px; overflow:hidden; position:absolute; left:50%; top:50%; margin-left:-301px; margin-top:-208px;}
-	.login_iptbox{bottom:90px;_bottom:72px;color:#FFFFFF;font-size:12px;height:30px;left:50%;
-margin-left:-280px;position:absolute;width:560px; overflow:visible;}
-	.login_iptbox .ipt{height:24px; width:110px; margin-right:22px; color:#fff; background:url(<?php echo IMG_PATH?>admin_img/ipt_bg.jpg) repeat-x; *line-height:24px; border:none; color:#000; overflow:hidden;}
-	<?php if(SYS_STYLE=='en'){ ?>
-	.login_iptbox .ipt{width:100px; margin-right:12px;}
-	<?php }?>
-	.login_iptbox label{ *position:relative; *top:-6px;}
-	.login_iptbox .ipt_reg{margin-left:12px;width:46px; margin-right:16px; background:url(<?php echo IMG_PATH?>admin_img/ipt_bg.jpg) repeat-x; *overflow:hidden;text-align:left;padding:2px 0 2px 5px;font-size:16px;font-weight:bold;}
-	.login_tj_btn{ background:url(<?php echo IMG_PATH?>admin_img/login_dl_btn.jpg) no-repeat 0px 0px; width:52px; height:24px; margin-left:16px; border:none; cursor:pointer; padding:0px; float:right;}
-	.yzm{position:absolute; background:url(<?php echo IMG_PATH?>admin_img/login_ts140x89.gif) no-repeat; width:140px; height:89px;right:56px;top:-96px; text-align:center; font-size:12px; display:none;}
-	.yzm a:link,.yzm a:visited{color:#036;text-decoration:none;}
-	.yzm a:hover{color:#C30;}
-	.yzm img{cursor:pointer; margin:4px auto 7px; width:130px; height:50px; border:1px solid #fff;}
-	.cr{font-size:12px;font-style:inherit;text-align:center;color:#ccc;width:100%; position:absolute; bottom:58px;}
-	.cr a{color:#ccc;text-decoration:none;}
-</style>
+    <meta charset=utf-8>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>后台登陆</title>
+    <meta name="author" content="叶子飞"/>
+    <style type="text/css">
+        body,div,dl,dt,dd,ul,ol,li,h1,h2,h3,h4,h5,code,form,input,button,textarea,p,th,td { margin:0; padding:0; outline:none; }
+        html,body { width: 100%; height: 100%; overflow: hidden; position: relative; }
+        li { list-style: none; }
+        .login-wrap { width: 100%; height: 260px; background: #f6f6f6; position: absolute; top: 38.2%; }
+        .login-show { position: absolute; right: 61.8%; top: 38.2%; }
+        .login { position: absolute; left: 61.8%; padding-top: 45px; }
+        .input-text input { width: 210px; height: 30px; margin-bottom: 10px; border: 3px solid #e6e6e6; border-radius: 4px; font-size: 16px; font-family: "Microsoft yahei",Tahoma,Arial,"Helvetica Neue","Hiragino Sans GB",Simsun,sans-self; text-indent: 18px; color: #333; overflow: hidden; }
+        .input-text input:hover,.input-text input:focus { border-color: #66afe9; }
+        .input-text input.error { border-color: #843534; }
+        .input-captcha input { width: 100px; float: left; }
+        .input-captcha .captcha-img { float: right; }
+        #login { width: 216px; height: 40px; border: none; cursor: pointer; font-size: 16px; font-family: "Microsoft yahei",Tahoma,Arial,"Helvetica Neue","Hiragino Sans GB",Simsun,sans-self; color: #666; border-radius: 4px; background: #e6e6e6; letter-spacing: 2px; }
+        #login:hover,#login:focus { background: #66afe9; color: #fff; }
+    </style>
 <script language="JavaScript">
 <!--
 	if(top!=self)
@@ -34,12 +30,36 @@ margin-left:-280px;position:absolute;width:560px; overflow:visible;}
 
 <body onload="javascript:document.myform.username.focus();">
 <div id="login_bg" class="login_box">
-	<div class="login_iptbox">
-   	 <form action="index.php?m=admin&c=index&a=login&dosubmit=1" method="post" name="myform"><input name="dosubmit" value="" type="submit" class="login_tj_btn" /><label><?php echo L('username')?>：</label><input name="username" type="text" class="ipt" value="" /><label><?php echo L('password')?>：</label><input name="password" type="password" class="ipt" value="" /><label><?php echo L('security_code')?>：</label><input name="code" type="text" class="ipt ipt_reg" onfocus="document.getElementById('yzm').style.display='block'" />
-    <div id="yzm" class="yzm"><?php echo form::checkcode('code_img')?><br /><a href="javascript:document.getElementById('code_img').src='<?php echo SITE_PROTOCOL.SITE_URL.WEB_PATH;?>api.php?op=checkcode&m=admin&c=index&a=checkcode&time='+Math.random();void(0);"><?php echo L('click_change_validate')?></a></div>
-     </form>
+    <div class="login-wrap">
+        <div class="login-show">
+            <img src="<?php echo IMG_PATH?>simleaf/img/logo.png">
+        </div>
+        <div class="login">
+        	<form action="index.php?m=admin&c=index&a=login&dosubmit=1" method="post" name="myform">
+	            <ul>
+	                <li class="input-text">
+	                    <label for="account"></label>
+	                    <input type="text" name="username" id="account" placeholder="账号">
+	                </li>
+	                <li class="input-text">
+	                    <label for="password"></label>
+	                    <input type="password" name="password" id="password" placeholder="密码">
+	                </li>
+	                <li class="input-text input-captcha">
+	                    <label for="captcha"></label>
+	                    <input type="text" name="code" id="captcha" placeholder="验证码">
+	                    <div class="captcha-img">
+	                        <?php echo form::checkcode('code_img')?>
+                    		<a href="javascript:document.getElementById('code_img').src='<?php echo SITE_PROTOCOL.SITE_URL.WEB_PATH;?>api.php?op=checkcode&m=admin&c=index&a=checkcode&time='+Math.random();void(0);"></a>
+	                    </div>
+	                </li>
+	                <li>
+	                	<input type="submit" name="dosubmit" class="sub" id="login" value="登陆" />
+	                </li>
+	            </ul>
+            </form> 
+        </div>
     </div>
-    <div class="cr"><?php echo L("copyright")?></div>
 </div>
 </body>
 </html>
