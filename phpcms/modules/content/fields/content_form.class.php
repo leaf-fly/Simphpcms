9ef83371731ec0276b2fb0ccb4b7f9d044d35b90
simleaf <814183676@qq.com>
2015-03-03 00:30:05 +0800

    revise 'ueditor'  insert 'html' code block 'htmlencode' error

--- phpcms/modules/content/fields/content_form.class.php
+++ phpcms/modules/content/fields/content_form.class.php
@@ -26,7 +26,8 @@ class content_form {
 				if($v['iscore'] || !$v['isadd'] || check_in($_groupid, $v['unsetgroupids'])) continue;
 			}
 			$func = $v['formtype'];
-			$value = isset($data[$field]) ? new_html_special_chars($data[$field]) : '';
+			$value = isset($data[$field]) ? $data[$field] : '';
+			// $value = isset($data[$field]) ? new_html_special_chars($data[$field]) : '';
 			if($func=='pages' && isset($data['maxcharperpage'])) {
 				$value = $data['paginationtype'].'|'.$data['maxcharperpage'];
 			}
